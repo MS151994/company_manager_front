c7bbba11602d0530d00bf2774c4bc4729a29a91c
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-04 23:59:58 +0200

    Clear components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {CookiesProvider} from "react-cookie";
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./components/HomePage";
+import {UserRegistration} from "./components/usersForm/UserRegistration";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <div>
+            <CookiesProvider>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/user/registration" element={<UserRegistration/>}/>
+                </Routes>
+            </CookiesProvider>
+        </div>
+    );
 }
 
-export default App;
