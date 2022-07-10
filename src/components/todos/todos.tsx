02bb2117feb89d08c5c4f9c58e42fa1e512711b7
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-10 23:37:18 +0200

    added todos component.

--- src/components/todos/todos.tsx
+++ src/components/todos/todos.tsx
@@ -1,8 +1,27 @@
 import {PageTitle} from "../commons/PageTitle/PageTitle";
+import {AddNewTodoForm} from "./AddNewTodoForm/AddNewTodoForm";
+
+import './todos.css';
 
 export const Todos = () => {
     return (
-        <PageTitle pageTitle={"my todo's"}/>
-
+        <div className={'todos__container'}>
+            <PageTitle pageTitle={"my todo's"}/>
+            <AddNewTodoForm/>
+            <div className="todo__item_box">
+                <h2>todo's</h2>
+                <div className="one_todo">
+                   <div className="info_box">
+                       <p>title</p>
+                       <p>text</p>
+                   </div>
+                    <div className="date_box">
+                        <p>added at: 15.07.2022</p>
+                        <p>deadline: 16.07.2022</p>
+                    </div>
+                    <button>✅</button>
+                </div>
+            </div>
+        </div>
     )
 }
