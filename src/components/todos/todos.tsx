d81b5de62c9d44650485e69682cce42ddefc1e67
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-11 23:34:20 +0200

    added todo from DB.

--- src/components/todos/todos.tsx
+++ src/components/todos/todos.tsx
@@ -1,26 +1,72 @@
 import {PageTitle} from "../commons/PageTitle/PageTitle";
 import {AddNewTodoForm} from "./AddNewTodoForm/AddNewTodoForm";
+import {useEffect, useState} from "react";
+import {TodosInterface} from "types";
+import {config} from "../config/config";
+import {useCookies} from "react-cookie";
+import {Spinner} from "../commons/Spinner/Spinner";
+import {OneTodo} from "./OneTodo/OneTodo";
 
 import './todos.css';
 
 export const Todos = () => {
+    const [todos, setTodos] = useState<TodosInterface[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [cookie, setCookie] = useCookies(['user'])
+    useEffect(() => {
+
+        (async () => {
+            try {
+                setLoading(true);
+                const res = await fetch(`${config.api}/todos/${cookie.user}`);
+                const data = await res.json();
+                await setTodos(data);
+            } finally {
+                setLoading(false);
+            }
+        })();
+
+    }, [])
+
     return (
         <div className={'todos__container'}>
             <PageTitle pageTitle={"my todo's"}/>
             <AddNewTodoForm/>
             <div className="todo__item_box">
-                <h2>todo's</h2>
-                <div className="one_todo">
-                   <div className="info_box">
-                       <p>title</p>
-                       <p>text</p>
-                   </div>
-                    <div className="date_box">
-                        <p>added at: 15.07.2022</p>
-                        <p>deadline: 16.07.2022</p>
-                    </div>
-                    <button>✅</button>
+                <div className="todo__separator">
+                    <p>Active</p>
                 </div>
+                {loading && <Spinner/>}
+                {todos.map(todo =>
+                    <OneTodo
+                        key={todo.id}
+                        id={todo.id}
+                        title={todo.title}
+                        text={todo.text}
+                        createdAt={todo.createdAt}
+                        deadline={todo.deadline}
+                        highPriority={todo.highPriority}
+                        userId={todo.userId}
+                        isActive={todo.isActive}
+                    />)}
+            </div>
+            <div className="todo__item_box">
+                <div className="todo__separator">
+                    <p>done</p>
+                </div>
+                {loading && <Spinner/>}
+                {todos.map(todo =>
+                    <OneTodo
+                        key={todo.id}
+                        id={todo.id}
+                        title={todo.title}
+                        text={todo.text}
+                        createdAt={todo.createdAt}
+                        deadline={todo.deadline}
+                        highPriority={todo.highPriority}
+                        userId={todo.userId}
+                        isActive={todo.isActive}
+                    />)}
             </div>
         </div>
     )
