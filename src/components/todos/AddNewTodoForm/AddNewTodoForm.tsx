191b09640e8098c7c71d6e04ad66ebc367b5a2b1
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-11 21:33:44 +0200

    added todos form component.

--- src/components/todos/AddNewTodoForm/AddNewTodoForm.tsx
+++ src/components/todos/AddNewTodoForm/AddNewTodoForm.tsx
@@ -1,26 +1,65 @@
 import {FormButton} from "../../commons/buttons/FormButon";
+import {useState} from "react";
 
 import './addNewTodoForm.css';
 
+
 export const AddNewTodoForm = () => {
+    const minDate = new Date().toISOString().slice(0, 10);
+
+    const [form, setForm] = useState({
+        title: '',
+        text: '',
+        deadline: minDate,
+        highPriority: false,
+    })
+
+    const updateForm = (key: string, value: string | boolean) => {
+        setForm(form => ({
+            ...form,
+            [key]: value,
+        }))
+    };
+
     return (
         <div className="add_new_todo">
             <form>
                 <label>
-                    <input type="text" placeholder={'title'}/>
+                    <input
+                        type="text"
+                        placeholder={'title'}
+                        required
+                        value={form.title}
+                        onChange={(e) => updateForm('title', e.target.value)}
+                    />
                 </label>
                 <label>
-                    <input type="text" placeholder={'text'}/>
+                    <input
+                        type="text"
+                        placeholder={'text'}
+                        required
+                        value={form.text}
+                        onChange={(e) => updateForm('text', e.target.value)}
+                    />
                 </label>
                 <label>
                     deadline:
-                    <input type="date"/>
+                    <input
+                        type="date"
+                        min={minDate}
+                        required
+                        value={form.deadline}
+                        onChange={(e) => updateForm('deadline', e.target.value)}
+                    />
                 </label>
-                <label className={'checkBoxLabel'}>
+                <label
+                    className={'checkBoxLabel'}>
                     high priority
                     <input
                         className={'checkBox'}
                         type="checkbox"
+                        checked={form.highPriority}
+                        onChange={(e) => updateForm('highPriority', e.target.checked)}
                     />
                 </label>
                 <FormButton buttonName={'add'}/>
