b09acd7131048dde28a675c5a59eb986cece8d30
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-07-15 02:55:11 +0200

    added navlink to item

--- src/components/HomePage/OneItem/OneItem.tsx
+++ src/components/HomePage/OneItem/OneItem.tsx
@@ -1,10 +1,15 @@
 import {SimpleInfoTask} from "types";
 import './oneItem.css';
+import {NavLink} from "react-router-dom";
 
 export const OneItem = (props: SimpleInfoTask) => {
     return (
         <li className={'oneItem__box'}>
-            {props.title}: {props.text}
+            <div>
+                <p>{props.title}</p>
+                <p> {props.text}</p>
+            </div>
+            <NavLink to={'/tasks'}>➡️</NavLink>
         </li>
     )
 }
