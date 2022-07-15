3c31d616baf10278eed8a8fdcb7101759856afe9
Maciek <maciej.stepien1994@gmail.com>
2022-07-15 12:34:42 +0200

    refactor one elemnt tyle and add opcional status

--- src/components/HomePage/OneItem/OneItem.tsx
+++ src/components/HomePage/OneItem/OneItem.tsx
@@ -2,12 +2,17 @@ import {SimpleInfoTask} from "types";
 import './oneItem.css';
 import {NavLink} from "react-router-dom";
 
-export const OneItem = (props: SimpleInfoTask) => {
+interface Props extends SimpleInfoTask {
+    isDone?: string | boolean;
+}
+
+export const OneItem = (props: Props) => {
     return (
-        <li className={'oneItem__box'}>
+        <li className={props.isDone === '1' ? "oneItem__box doneTask" : 'oneItem__box'}>
             <div>
                 <p>{props.title}</p>
                 <p> {props.text}</p>
+                {props.isDone === "1" ? <p>Done</p> : <p>in progress</p>}
             </div>
             <NavLink to={'/tasks'}>➡️</NavLink>
         </li>
