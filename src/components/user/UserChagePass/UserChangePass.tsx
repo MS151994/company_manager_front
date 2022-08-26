8b581dac4c387b282ccb404cf803c61a6069f7f6
Maciej Stępień <maciej.stepien1994@gmail.com>
2022-08-27 00:07:06 +0200

    Add functionality to user component and refactor style

--- src/components/user/UserChagePass/UserChangePass.tsx
+++ src/components/user/UserChagePass/UserChangePass.tsx
@@ -1,15 +1,21 @@
-import {ChangePassContainer, ChangePassForm} from "./UserChangePass.styles";
 import {FormButton} from "../../commons/buttons/FormButon";
 import {AiOutlineClose} from "react-icons/ai";
 import {FaUserEdit} from "react-icons/fa";
 import {SyntheticEvent, useState} from "react";
+import {UserModalContainer, UserModalStyles} from "../../../const/layout/UserModal.styles";
+import {useColorMode, useToast} from "@chakra-ui/react";
+import {config} from "../../config/config";
 
 interface Props {
-    isActive: (arg: boolean) => void
+    isOpen: (arg: boolean) => void
     userId: string
+    refreshData: () => void
 }
 
-export const UserChangePass = ({isActive}: Props) => {
+export const UserChangePass = ({isOpen, userId, refreshData}: Props) => {
+    const [loading, setLoading] = useState<boolean>(false)
+    const {colorMode} = useColorMode();
+    const toast = useToast();
     const [form, setForm] = useState({
         oldPass: '',
         newPass: '',
@@ -22,19 +28,53 @@ export const UserChangePass = ({isActive}: Props) => {
         }))
     }
 
-    const handleSubmit = (e: SyntheticEvent) => {
+    const handleSubmit = async (e: SyntheticEvent) => {
         e.preventDefault();
-        console.log(form)
+        try {
+            setLoading(true)
+            const res = await fetch(`${config.api}/user/changepassword`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    oldPass: form.oldPass,
+                    newPass: form.newPass,
+                    userId: userId
+                })
+            })
+            if (res.status === 401) {
+                toast({
+                    title: `Wrong old password`,
+                    status: 'warning',
+                    duration: 3000,
+                    isClosable: true,
+                });
+            }
+            if (res.status === 200) {
+                toast({
+                    title: `password updated!`,
+                    status: 'success',
+                    duration: 3000,
+                    isClosable: true,
+                });
+            }
+
+        } finally {
+            setLoading(false)
+            await refreshData()
+            isOpen(false)
+        }
     };
 
     return (
-        <ChangePassContainer>
-            <ChangePassForm onSubmit={handleSubmit}>
-                <div>
-                    <FaUserEdit/>
-                    <h1> change pass</h1>
-                </div>
-                <div>
+        <UserModalStyles currentColor={colorMode}>
+            <UserModalContainer currentColor={colorMode}>
+                <form onSubmit={handleSubmit}>
+                    <div className={'modalIconTitle'}>
+                        <FaUserEdit/>
+                        <h1> change pass</h1>
+                    </div>
                     <label htmlFor="">
                         <input
                             type="password"
@@ -53,10 +93,10 @@ export const UserChangePass = ({isActive}: Props) => {
                             onChange={(e) => updateForm('newPass', e.target.value)}
                         />
                     </label>
-                </div>
-                <FormButton buttonName={'change'}/>
-                <AiOutlineClose onClick={() => isActive(false)} className={'closeIcon'}/>
-            </ChangePassForm>
-        </ChangePassContainer>
+                    <FormButton buttonName={'change'}/>
+                    <AiOutlineClose onClick={() => isOpen(false)} className={'closeIcon'}/>
+                </form>
+            </UserModalContainer>
+        </UserModalStyles>
     )
 }
